32eb4d68fe2578b26b984ee2c32fd19edea8ec00
Denis Mitlenko <mitlenkodenis3@gmail.com>
2026-01-14 00:39:37 +0100

    add token package with token definition

---
+++ token/token.go
@@ -0,0 +1,33 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + literal
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "ASSIGN"
+	PLUS   = "PLUS"
+
+	// Delimeters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
